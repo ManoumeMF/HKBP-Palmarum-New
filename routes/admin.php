5a93240afd0622a999e6631e0ba28da9d01b7e2e
ManoumeMF <143568027+ManoumeMF@users.noreply.github.com>
2024-09-10 08:51:04 +0700

    new update ardiles

--- routes/admin.php
+++ routes/admin.php
@@ -192,10 +192,3 @@ Route::post("/jemaat/update/{id}", [JemaatController::class, 'update'])->name('J
 Route::delete("/jemaat/hapus", [JemaatController::class, 'delete'])->name('Jemaat.delete');
 Route::get("/jemaat/detail", [JemaatController::class, 'detail'])->name('Jemaat.detail');
 
-<<<<<<< Updated upstream
-=======
-//Route untuk DropdownLokasi
-Route::get("/kota", [DropdownLokasiContoller::class, 'kota'])->name('DropdownLokasi.kota');
-Route::get("/kecamatan", [DropdownLokasiContoller::class, 'kecamatan'])->name('DropdownLokasi.kecamatan');
-Route::get("/kelurahan", [DropdownLokasiContoller::class, 'kelurahan'])->name('DropdownLokasi.kelurahan');
->>>>>>> Stashed changes
